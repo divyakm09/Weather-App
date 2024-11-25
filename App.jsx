bf256daf85acf14854b3ef44bfe7a54b64921e55
divyakm09 <147162320+divyakm09@users.noreply.github.com>
2024-11-25 08:49:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import Weather from './components/Weather'
+
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <Weather/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+import Weather from "./components/Weather"
+
+const App = () => {
+  return (
+    <div  className='app'>
+    <Weather/>
+      
+    </div>
+  )
+}
+
+export default App
